6577c3002a834de157d0912ed578028ad80aae93
dev.amit.dixit <dev.amit.dixit@gmail.com>
2023-12-25 16:23:01 +0530

    traits oops

---
+++ traits_sample_oops/src/main.rs
@@ -0,0 +1,35 @@
+use traits_sample_oops::{Button, Draw, Screen};
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("yes"),
+                    String::from("no"),
+                    String::from("maybe"),
+                ],
+            }),
+            Box::new(Button {
+                width: 10,
+                height: 5,
+                label: String::from("Submit"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("SelectBox  Drawn");
+    }
+}
